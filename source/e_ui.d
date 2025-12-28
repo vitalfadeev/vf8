7eb5cfb4a1bcc20306b60cbc5883bb2bf4ea0d3d
Vital Fadeev <vital.fadeev@gmail.com>
2025-12-28 14:22:40 +0700

    upd

--- source/e_ui.d
+++ source/e_ui.d
@@ -221,8 +221,11 @@ union {
 Klass
 window = {
     cast (GO) (O* o, E* e, Ex* ex, void* evt) {
-        (cast (E_ui*) e).x = 0;
-        
+        with (cast (E_ui*) e) {
+            x = 0;
+            y = 100;
+        }
+
         with (cast (Klass*) ex) {
 //        if ((cast (Event*) evt).type == UPDATE) {
 //            // (cast (Event*) evt).props[x] = 0
